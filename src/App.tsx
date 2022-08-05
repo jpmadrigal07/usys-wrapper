82d4360aadca18b81b23d2cf1fdbbc41717d0fbf
John Patrick Madrigal <jp.madrigal07@gmail.com>
2022-08-05 15:41:32 +0800

    fix: module federation bugs for ts, eslint and bootloader

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
+import Header from 'cashier/Header'
 
-export const App = () => {
+export default function App() {
   const [first, setFirst] = useState(0)
   return (
     <div>
       <p className="underline text-red-600 font-bold">Number {first}</p>
       <button onClick={() => setFirst(first + 1)}>Click</button>
+      <Header />
     </div>
   )
 }
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,8 +1,9 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { ModuleFederationPlugin } = require('webpack').container
 
 module.exports = {
-  entry: path.resolve(__dirname, '..', 'src/index.tsx'),
+  entry: path.resolve(__dirname, '..', 'src/entry.tsx'),
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
   },
@@ -16,6 +17,13 @@ module.exports = {
             loader: 'babel-loader',
           },
         ],
+      },
+      {
+        test: /\.m?js/,
+        type: 'javascript/auto',
+        resolve: {
+          fullySpecified: false,
+        },
       },
       {
         test: /\.css$/,
@@ -34,8 +42,18 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '..', './build'),
     filename: 'bundle.js',
+    publicPath: 'http://localhost:4000/',
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: 'wrapper',
+      filename: 'remoteEntry.js',
+      remotes: {
+        cashier: 'cashier@http://localhost:4001/remoteEntry.js',
+      },
+      exposes: {},
+      shared: {},
+    }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', 'public/index.html'),
     }),
